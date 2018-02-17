3b8026283a5a0a722bbe71efcfbe9c841aa0fd68
red-ant-628 <red.ant.628@gmail.com>
2018-02-17 17:55:35 -0600

    Update CC.js

--- CC.js
+++ CC.js
@@ -4,16 +4,3 @@ document.getElementById('cookie').onclick = function() {
   cookies += 1
   document.getElementById('num_cookies').innerHTML = cookies
 }
-
-window.onscroll = function() {myFunction()};
-var header = document.getElementById("myHeader");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
